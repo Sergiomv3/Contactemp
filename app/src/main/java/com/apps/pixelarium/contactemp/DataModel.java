8f3e36e88d339c2e338d2693bb3fd2213c5343bb
smarti42 <sergio-martin@t-systems.com>
2017-08-02 12:42:50 +0200

    Temporizer activity

--- app/src/main/java/com/apps/pixelarium/contactemp/DataModel.java
+++ app/src/main/java/com/apps/pixelarium/contactemp/DataModel.java
@@ -11,9 +11,20 @@ public class DataModel implements Comparable<DataModel>, Serializable{
     private String name;
     private String number;
 
+    public boolean isProgrammed() {
+        return isProgrammed;
+    }
+
+    public void setProgrammed(boolean programmed) {
+        isProgrammed = programmed;
+    }
+
+    private boolean isProgrammed;
+
     public DataModel(String name, String number) {
         this.name = name;
         this.number = number;
+        this.isProgrammed = false;
     }
 
     public String getName() {
--- app/src/main/java/com/apps/pixelarium/contactemp/TemporizerActivity.java
+++ app/src/main/java/com/apps/pixelarium/contactemp/TemporizerActivity.java
@@ -1,14 +1,21 @@
 package com.apps.pixelarium.contactemp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActionBar;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Toolbar;
 
-public class TemporizerActivity extends AppCompatActivity {
+public class TemporizerActivity extends Activity {
     private DataModel dataModel;
     private TextView textviewName;
     private TextView textviewNumber;
+    private CheckBox checkBox;
+    private DatePicker datePicker;
+    private ActionBar actionBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,9 +23,33 @@ public class TemporizerActivity extends AppCompatActivity {
         dataModel = (DataModel) getIntent().getSerializableExtra("dataModel");
         textviewName = (TextView)findViewById(R.id.textview_name);
         textviewNumber = (TextView)findViewById(R.id.textview_number);
+        checkBox = (CheckBox)findViewById(R.id.checkBox);
+        datePicker = (DatePicker)findViewById(R.id.datePicker);
 
         /*Print data contact*/
         textviewName.setText(dataModel.getName());
         textviewNumber.setText(dataModel.getNumber());
+        
+        checkBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // isChecked means that the check button is activated now
+                if(checkBox.isChecked()) {
+                    datePicker.setVisibility(View.VISIBLE);
+                }else{
+                    datePicker.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        setUpToolbar();
+
+    }
+
+    private void setUpToolbar() {
+        actionBar = getActionBar();
+
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
     }
 }
